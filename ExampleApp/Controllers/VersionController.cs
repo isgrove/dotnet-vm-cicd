88d591446df2d47a6622c7ef53f3570013f1f5c7
sam isgrove <samis@hey.com>
2024-01-17 15:32:47 +1100

    feat: added example app

---
+++ ExampleApp/Controllers/VersionController.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExampleApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VersionController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string version = string.Empty;
+            string filePath = "./version.txt";
+
+            using StreamReader reader = new(filePath);
+            string? line = reader.ReadLine();
+
+            while (line != null)
+            {
+                version += line;
+                line = reader.ReadLine();
+            }
+
+            return Ok(version);
+        }
+    }
+}
